12ecde040e702d95a330b3159969d763ddaa7fe8
Alan Kelly <alan.kelly.london@gmail.com>
2021-04-14 20:52:41 +0100

    add enums for class and method

---
+++ StunClass.cs
@@ -0,0 +1,10 @@
+namespace Stun
+{
+    public enum StunClass
+    {
+        Request = 0,
+        Indication = 1,
+        Success = 2,
+        Error = 3
+    }
+}
---
+++ StunMethod.cs
@@ -0,0 +1,9 @@
+namespace Stun
+{
+    public enum StunMethod
+    {
+        Reserved = 0,
+        Binding = 1,
+        SharedSecretReserved = 2
+    }
+}
